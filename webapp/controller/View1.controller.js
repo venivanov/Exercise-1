30dd348d832a3f34d1426a50f6e247aad85b6f05
venivanov <ventsislav.n.ivanov@gmail.com>
2020-12-07 15:26:08 +0000

    very weird

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,20 +1,20 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-   "sap/m/MessageToast",
-   "sap/ui/model/json/JSONModel"
-], function (Controller, MessageToast, JSONModel){
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
 	"use strict";
 	return Controller.extend("com.exercise1.Exercise1.controller.View1", {
 		onInit: function () {
 			var data = {
-				recipient : {
-					name : "World"
+				recipient: {
+					name: "World"
 				}
 			};
-			var model = new JSONModel (data);
+			var model = new JSONModel(data);
 			this.getView().setModel(model);
 		},
-		onShowHello : function () {
+		onShowHello: function () {
 			MessageToast.show("Hello World!");
 		}
 	});
